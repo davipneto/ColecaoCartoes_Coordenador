b9a9811fe3b1732a080b49f96fc6b87cc6e25e6b
Davi Pereira Neto <davineto@alunos.utfpr.edu.br>
2017-11-22 14:13:14 -0200

    criacao do projeto

---
+++ src/colecaocartoes_coordenador/ColecaoCartoes_Coordenador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colecaocartoes_coordenador;
+
+/**
+ *
+ * @author davi
+ */
+public class ColecaoCartoes_Coordenador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
